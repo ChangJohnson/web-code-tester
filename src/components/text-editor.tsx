f38303d76f8a83a50defd8c81d4d612be8492b49
UmbrellaOsJohnson <umbrellaosjohnson@gmail.com>
2023-11-16 14:41:28 -0500

    working on Text editor to have a preview

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,46 @@
+import MDEditor from '@uiw/react-md-editor';
+import './text-editor.css';
+import { useState, useEffect, useRef } from 'react';
+
+const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+
+    // this even bubbles up to html so where ever you click this function us called so above we set up so in the code editor it does get called.
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className='text-editor' ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+      </div>
+    );
+  }
+  return (
+    <div className='text-editor card' onClick={() => setEditing(true)}>
+      <div className='card-content'>
+        <MDEditor.Markdown source={value} />
+      </div>
+    </div>
+  );
+};
+
+export default TextEditor;
