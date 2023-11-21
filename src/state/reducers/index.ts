f8103f78792c3db93fad89ccb04119481dc3af38
UmbrellaOsJohnson <umbrellaosjohnson@gmail.com>
2023-11-21 15:04:18 -0500

    made a redux in state folder

---
+++ src/state/reducers/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import cellsReducer from './cellsReducers';
+
+const reducers = combineReducers({ cells: cellsReducer });
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
