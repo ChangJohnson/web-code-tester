cc5cf41cc49031e4165868b6fcd8dbf647372cfd
UmbrellaOsJohnson <umbrellaosjohnson@gmail.com>
2023-11-23 14:30:14 -0500

    changed some css and added components, add-cell and action-bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './state';
 // import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
+// import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
         {/* <CodeCell /> */}
-        <TextEditor />
+        {/* <TextEditor /> */}
+        <CellList />
       </div>
     </Provider>
   );
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -4,7 +4,7 @@ import {
   UpdateCellAction,
   DeleteCellAction,
   MoveCellAction,
-  InsertCellBeforeAction,
+  InsertCellAfterAction,
   Direction,
 } from '../actions';
 import { CellTypes } from '../cell';
@@ -30,12 +30,12 @@ export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   return { type: ActionType.MOVE_CELL, payload: { id, direction } };
 };
 
-export const insertCellBefore = (
-  id: string,
+export const insertCellAfter = (
+  id: string | null,
   cellType: CellTypes
-): InsertCellBeforeAction => {
+): InsertCellAfterAction => {
   return {
-    type: ActionType.INSERT_CELL_BEFORE,
+    type: ActionType.INSERT_CELL_AFTER,
     payload: { id, type: cellType },
   };
 };
